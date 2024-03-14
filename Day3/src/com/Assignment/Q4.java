36a8fcaaf5d3755655e392a9f932e993ca4cf235
Soham Ingale <91049670+SohamIngale@users.noreply.github.com>
2024-03-14 23:24:28 +0530

    Add files via upload

--- Day3/src/com/Assignment/Q4.java
+++ Day3/src/com/Assignment/Q4.java
@@ -3,8 +3,13 @@ package com.Assignment;
 public class Q4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i = 10;
+		float f = 20.25f;
+		double d = 1234.222d;
+		int x = 245;
+		//boolean bX = (x!=0); //isse possible hai but dont focus much on this
+		String str = Integer.toString(i);
+		System.out.println(str);
 	}
 
 }
